c1786a83017424394028ff6e89afbc6461f0276d
9Nia <syakuronabdan@live.com>
2018-06-09 19:01:05 +0700

    turn off sequelize logging

--- src/modules/core/sequelize.js
+++ src/modules/core/sequelize.js
@@ -10,11 +10,12 @@ function connect(config) {
   const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
-    port: config.port
+    port: config.port,
+    logging: config.logging
   });
   return sequelize;
 }
 
-const db = connect(cfg.sequelize);
+const db = connect(cfg.db);
 
 module.exports = { db };
