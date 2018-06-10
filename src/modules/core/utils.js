81291c5b0cc44f7651e82c449781f25f8828fadf
9Nia <syakuronabdan@live.com>
2018-06-10 07:45:37 +0700

    add map prop to db util

--- src/modules/core/utils.js
+++ src/modules/core/utils.js
@@ -4,5 +4,18 @@
  */
 const wrap = fn => (req, res, next) => fn(req, res, next).catch(e => next(e));
 
-module.exports = { wrap };
+/**
+ * Transform supplied data properties to match with db column
+ * @param input {object}
+ * @param columnName {object}
+ * @returns {object}
+ */
+const matchDB = (input, columnName) => {
+  return Object.keys(input).reduce((matched, prop) => {
+    if (columnName[prop] && input[prop] !== undefined) matched[columnName[prop]] = input[prop];
+    return matched;
+  }, {});
+}
+
+module.exports = { wrap, matchDB };
 
